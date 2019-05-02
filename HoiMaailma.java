83808e2afb2f5e3f49d7e5c0648b0a496b40dd2d
Mika Stenberg <mika.stenberg@laurea.fi>
2019-05-02 09:46:03 +0300

    Add files via upload

---
+++ HoiMaailma.java
@@ -0,0 +1,13 @@
+
+public class HoiMaailma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hoi Maailma");
+		System.out.println("Hoi Maailma");
+		System.out.print("Hoi Maailma");
+		System.out.print("Hoi Maailma");
+		System.out.println("Hoi Maailma");
+		
+	}
+}
